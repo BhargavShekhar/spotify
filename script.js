e7637d5a81597f77bc813a611e5b170546a53611
Bhargav Shekhar <70209326+BhargavShekhar@users.noreply.github.com>
2024-02-26 18:10:37 +0530

    Update script.js
    trying to fetch songs from github server

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("Lest make it goooood with some scripting babyyyyyy!!!!");
 
 async function main() {
-    let a = await fetch("http://127.0.0.1:5500/Spotify/media/songs/")
+    // let a = await fetch("http://127.0.0.1:5500/Spotify/media/songs/")
+    let a = await fetch("https://bhargavshekhar.github.io/spotify/media/songs/")
     let responce = await a.text();
     console.log(responce);
     console.log(a);
